94fe1fd5d54c0be530ae1e5612f6522ded600757
escoobi <gustavo_o_@hotmail.com>
2022-02-14 14:13:00 -0400

    Criado a Class Livro

--- src/com/dio/Main.java
+++ src/com/dio/Main.java
@@ -9,5 +9,54 @@ public class Main {
         Dog dog = new Dog();
 
         System.out.printf(dog.toString());
+
+        Livro livro = new Livro("O Investidor Inteligente", 2021, 623);
+        System.out.println(livro);
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer ano;
+    private Integer npg;
+
+
+    public Livro(String nome, Integer ano, Integer npg) {
+        this.nome = nome;
+        this.ano = ano;
+        this.npg = npg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Integer getNpg() {
+        return npg;
+    }
+
+    public void setNpg(Integer npg) {
+        this.npg = npg;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", ano=" + ano +
+                ", npg=" + npg +
+                '}';
     }
 }
