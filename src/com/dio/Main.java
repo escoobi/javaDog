e8bd3ce85e112181147b72602fbba7da3860b432
escoobi <gustavo_o_@hotmail.com>
2022-02-15 08:46:37 -0400

    1 — Pré e Pos. 2 — Aritmético. 3 — Atribuição. 4 — Precedência.

--- src/com/dio/Main.java
+++ src/com/dio/Main.java
@@ -7,6 +7,39 @@ public class Main {
     public static void main(String[] args) {
         /* System.out.printf("Estamos aqui!");*/
 
+        System.out.println();
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 10000;
+        //short s2 = 40000;
+        //int i1 = -10000000000;
+        int i2 = 1000000000;
+
+        long l1 = 1000000000000000000l;
+        //long l2 = 10000000000000000000l;
+
+        //float f1 = 4.5;
+        float f2 = 4.5f;
+
+        //double d1 = 4.5;
+        double d2 = 4.5d;
+
+        char c1 = 'w';
+        //char c2 = 'tw';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Fulano Fulano Fulano";
+        String st3 = "Fulano Fulano";
+        String st4 = "Fulano";
+
+        Boolean bol1 = true;
+        Boolean bo2 = false;
+
+
+
         Dog dog = new Dog("Totó","Vira-Lata", 30, 5);
 
         System.out.println(dog.getNome());
@@ -14,6 +47,96 @@ public class Main {
         Livro livro = new Livro("O Investidor Inteligente", 2021, 623);
 
         System.out.println(livro);
+
+        System.out.println("PrePos");
+        prePos();
+        System.out.println("Aritimético");
+        aritmetico();
+        System.out.println("Atribuição");
+        atribuicao();
+        System.out.println("Precedência");
+        precedencia();
+    }
+
+    private static void precedencia() {
+        int i = 10;
+        int j = 20;
+        int k = 30;
+
+        int a = i++ + --j * k;
+        System.out.println("i++ + --j * k: " + a);
+        System.out.println("i: " + i);
+
+        int b = k / --i % 3 + 1;
+        System.out.println("k / --i % 3 + 1: " + b);
+        System.out.println("i: " + i);
+
+        int c = 2;
+        c *= i += 5;
+        System.out.println("c *= i += 5: " + c);
+
+
+    }
+
+    private static void atribuicao() {
+
+        int i = 1500;
+        short j = 15;
+        long l = 500L;
+        int k = 35;
+        float f = 3.5F;
+        double d= f;
+
+        System.out.println("d: " + d);
+
+        i += 5;
+        j -= 3;
+        d /= 2.7D;
+        l *= 3;
+        k %= 2;
+        System.out.println("i: " + i);
+        System.out.println("j: " + j);
+        System.out.println("d: " + d);
+        System.out.println("l: " + l);
+        System.out.println("k: " + k);
+
+        i = k = j;
+
+        System.out.println("k: " + k);
+        System.out.println("i: " + i);
+    }
+
+    private static void aritmetico() {
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = 50;
+
+        int r1 = a + b;
+        int r2 = c - a;
+        int r3 = d * b;
+        int r4 = e / a;
+        int r5 = c % b;
+
+        System.out.println("a + b: " + r1);
+        System.out.println("c - b: " + r2);
+        System.out.println("d * b: " + r3);
+        System.out.println("e / b: " + r4);
+        System.out.println("c % b: " + r5);
+    }
+
+    private static void prePos(){
+        int k = 10;
+
+        int i = ++k;
+        int j = k--;
+        int x = k;
+
+        System.out.println("i: " + i);
+        System.out.println("j: " + j);
+        System.out.println("x: " + x);
     }
 }
 
